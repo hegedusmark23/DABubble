ee6cd4d8b5cc52c2f57037094e722396889c05fb
joostheidrich <joost.heidrich@web.de>
2024-07-24 19:16:38 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./main/header/header.component";
+import { OpenSidebarComponent } from "./main/open-sidebar/open-sidebar.component";
+import { SidebarComponent } from "./main/sidebar/sidebar.component";
+import { ChannelComponent } from "./main/channel/channel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, OpenSidebarComponent, SidebarComponent, ChannelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
