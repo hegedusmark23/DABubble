7e2dde0e2341f25e27455f6d49be03b61066fe90
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-07-25 16:16:21 +0200

    good progress on log-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./main/header/header.component";
 import { OpenSidebarComponent } from "./main/open-sidebar/open-sidebar.component";
 import { SidebarComponent } from "./main/sidebar/sidebar.component";
 import { ChannelComponent } from "./main/channel/channel/channel.component";
+import { LogInComponent } from "./landing_page/log-in/log-in.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, OpenSidebarComponent, SidebarComponent, ChannelComponent],
+  imports: [RouterOutlet, HeaderComponent, OpenSidebarComponent, SidebarComponent, ChannelComponent, LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
