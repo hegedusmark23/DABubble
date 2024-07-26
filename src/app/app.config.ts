5634b610d36206bb2577aecb0476a398a62b3727
joostheidrich <joost.heidrich@web.de>
2024-07-26 17:44:19 +0200

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,31 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble-3c5b0',
+        appId: '1:904297976039:web:5a55b686aa74fb7763d242',
+        storageBucket: 'dabubble-3c5b0.appspot.com',
+        locationId: 'europe-west',
+        apiKey: 'AIzaSyC9dPnMg7uyONz8kydSLvm_zg6iLfp4NmM',
+        authDomain: 'dabubble-3c5b0.firebaseapp.com',
+        messagingSenderId: '904297976039',
+        measurementId: 'G-Z5QGG7XBD0',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
 };
