b7eef3998e2eefae2b96f2270e9e9466c846e972
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-29 09:07:40 +0200

    added edit-profil popup

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SearchFieldComponent } from './search-field/search-field.component';
 import { SidebarService } from '../../services/sidebar.service';
+import { ProfilOptionsPopupComponent } from './profil-options-popup/profil-options-popup.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchFieldComponent],
+  imports: [SearchFieldComponent, ProfilOptionsPopupComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
