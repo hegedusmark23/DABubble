a8694a383b90c1c50e80b8e5923c778090e08e99
joostheidrich <joost.heidrich@web.de>
2024-08-01 20:30:06 +0200

    thread img upload

--- src/app/main/thread/thread-chat-area/thread-chat-area.component.ts
+++ src/app/main/thread/thread-chat-area/thread-chat-area.component.ts
@@ -1,8 +1,164 @@
+// import {
+//   AfterViewInit,
+//   ChangeDetectorRef,
+//   Component,
+//   ElementRef,
+//   Input,
+//   OnInit,
+//   QueryList,
+//   ViewChild,
+//   ViewChildren,
+// } from '@angular/core';
+// import {
+//   Firestore,
+//   collection,
+//   deleteDoc,
+//   doc,
+//   limit,
+//   onSnapshot,
+//   query,
+// } from '@angular/fire/firestore';
+// import { CommonModule } from '@angular/common';
+// import { RouterModule, RouterOutlet } from '@angular/router';
+// import { Message } from '../../../../models/message.class';
+// import { ThreadService } from '../../../services/thread.service';
+// import { ChannelSelectionService } from '../../../services/channel-selection.service';
+// @Component({
+//   selector: 'app-thread-chat-area',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './thread-chat-area.component.html',
+//   styleUrl: './thread-chat-area.component.scss',
+// })
+// export class ThreadChatAreaComponent {
+//   @Input() threadId: any;
+
+//   allMessagesSorted: Message[] = [];
+//   allMessages: Message[] = [];
+//   currentChannel: any;
+
+//   scrolled = true;
+//   @ViewChild('messageContainer') private messageContainer?: ElementRef;
+//   containerClasses: { [key: string]: boolean } = {};
+
+//   @ViewChildren('messageList') messageLoaded!: QueryList<any>;
+
+//   @ViewChild('myDiv') myDiv!: ElementRef;
+
+//   constructor(
+//     private threadService: ThreadService,
+//     private firestore: Firestore,
+//     private channelSelectionService: ChannelSelectionService
+//   ) {}
+
+//   ngAfterViewInit(): void {
+//     this.subMessages();
+//     this.messageLoaded.changes.subscribe((t) => {
+//       if (this.scrolled) {
+//         this.scrolled = false;
+//         this.scrollToBottom();
+//       }
+//     });
+//   }
+
+//   subMessages() {
+//     const q = query(
+//       collection(
+//         this.firestore,
+//         'Channels',
+//         this.currentChannel,
+//         'messages',
+//         this.threadId,
+//         'thread'
+//       ),
+//       limit(1000)
+//     );
+//     onSnapshot(q, (list) => {
+//       this.allMessages = [];
+//       list.forEach((element) => {
+//         this.allMessages.push(this.setNoteObject(element.data(), element.id));
+//       });
+//       this.sortMessages();
+//     });
+//   }
+
+//   setNoteObject(obj: any, id: string): Message {
+//     return {
+//       id: id,
+//       message: obj.message || '',
+//       weekday: obj.weekday || '',
+//       year: obj.year || '',
+//       month: obj.month || '',
+//       day: obj.day || '',
+//       hour: obj.hour || '',
+//       minute: obj.minute || '',
+//       seconds: obj.seconds || '',
+//       milliseconds: obj.milliseconds || '',
+//       user: obj.user || '',
+//       fileUrl: obj.fileUrl || '',
+//       fileName: obj.fileName || '',
+//     };
+//   }
+
+//   sortMessages(): void {
+//     this.allMessagesSorted = [];
+//     this.allMessagesSorted = [...this.allMessages].sort((a, b) => {
+//       const dateA = new Date(
+//         a.year,
+//         a.month - 1,
+//         a.day,
+//         a.hour,
+//         a.minute,
+//         a.seconds
+//       );
+//       const dateB = new Date(
+//         b.year,
+//         b.month - 1,
+//         b.day,
+//         b.hour,
+//         b.minute,
+//         b.seconds
+//       );
+//       return dateA.getTime() - dateB.getTime(); // Älteste zuerst, daher umgekehrt
+//     });
+//   }
+
+//   scrollToBottom(): void {
+//     if (typeof window !== 'undefined') {
+//       // Browser-spezifischer Code hier
+//       const container = document.getElementById('messageContainer');
+//       if (container) {
+//         container.scrollTop = container.scrollHeight;
+//       }
+//     }
+//   }
+
+//   getFormattedTime(hour: any, minute: any): any {
+//     const hours = hour.toString().padStart(2, '0');
+//     const minutes = minute.toString().padStart(2, '0');
+//     return `${hours}:${minutes} Uhr`;
+//   }
+
+//   ngOnInit(): void {
+//     this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
+//       this.currentChannel = channel;
+//       this.onChannelChange(channel);
+//     });
+//   }
+
+//   onChannelChange(channel: string): void {
+//     // Deine Logik hier
+//     this.subMessages(); // Ensure messages are fetched on channel change
+//   }
+// }
+
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   ElementRef,
+  inject,
   Input,
   OnInit,
   QueryList,
@@ -23,6 +179,8 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { Message } from '../../../../models/message.class';
 import { ThreadService } from '../../../services/thread.service';
 import { ChannelSelectionService } from '../../../services/channel-selection.service';
+import { AuthService } from '../../../services/auth.service';
+
 @Component({
   selector: 'app-thread-chat-area',
   standalone: true,
@@ -30,35 +188,58 @@ import { ChannelSelectionService } from '../../../services/channel-selection.ser
   templateUrl: './thread-chat-area.component.html',
   styleUrl: './thread-chat-area.component.scss',
 })
-export class ThreadChatAreaComponent {
+export class ThreadChatAreaComponent implements OnInit, AfterViewInit {
   @Input() threadId: any;
+  authService = inject(AuthService);
 
+  allMessagesSortedDate: any = [];
   allMessagesSorted: Message[] = [];
   allMessages: Message[] = [];
-  currentChannel: any;
-
+  allDates: any = [];
+  dateCounter = 0;
   scrolled = true;
+  date = false;
   @ViewChild('messageContainer') private messageContainer?: ElementRef;
   containerClasses: { [key: string]: boolean } = {};
 
   @ViewChildren('messageList') messageLoaded!: QueryList<any>;
 
   @ViewChild('myDiv') myDiv!: ElementRef;
+  currentChannel = 'Entwicklerteam';
 
   constructor(
     private threadService: ThreadService,
     private firestore: Firestore,
-    private channelSelectionService: ChannelSelectionService
+    private channelSelectionService: ChannelSelectionService,
+    private cd: ChangeDetectorRef
   ) {}
 
+  ngOnInit(): void {}
+
   ngAfterViewInit(): void {
-    this.subMessages();
+    this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
+      this.currentChannel = channel;
+      this.onChannelChange(channel);
+    });
+
     this.messageLoaded.changes.subscribe((t) => {
       if (this.scrolled) {
         this.scrolled = false;
         this.scrollToBottom();
       }
     });
+    this.subMessages(); // Move subMessages to ngAfterViewInit to ensure View is initialized
+  }
+
+  openThread(thread: any) {
+    if (this.threadService.isThreadOpen()) {
+      this.threadService.closeThread();
+      setTimeout(() => {
+        this.threadService.openThread(thread);
+      }, 300);
+    } else {
+      this.threadService.openThread(thread);
+    }
   }
 
   subMessages() {
@@ -133,21 +314,40 @@ export class ThreadChatAreaComponent {
     }
   }
 
+  getMonthName(monthNumber: number): string {
+    const months: string[] = [
+      'Januar',
+      'Februar',
+      'März',
+      'April',
+      'Mai',
+      'Juni',
+      'Juli',
+      'August',
+      'September',
+      'Oktober',
+      'November',
+      'Dezember',
+    ];
+
+    if (monthNumber < 1 || monthNumber > 12) {
+      throw new Error(
+        'Ungültige Monatszahl. Bitte geben Sie eine Zahl zwischen 1 und 12 ein.'
+      );
+    }
+
+    return months[monthNumber - 1];
+  }
+
   getFormattedTime(hour: any, minute: any): any {
     const hours = hour.toString().padStart(2, '0');
     const minutes = minute.toString().padStart(2, '0');
     return `${hours}:${minutes} Uhr`;
   }
 
-  ngOnInit(): void {
-    this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
-      this.currentChannel = channel;
-      this.onChannelChange(channel);
-    });
-  }
-
   onChannelChange(channel: string): void {
-    // Deine Logik hier
-    this.subMessages(); // Ensure messages are fetched on channel change
+    setTimeout(() => {
+      this.subMessages(); // Ensure messages are fetched on channel change
+    }, 10);
   }
 }
--- src/app/main/thread/thread-message-input/thread-message-input.component.ts
+++ src/app/main/thread/thread-message-input/thread-message-input.component.ts
@@ -4,6 +4,7 @@ import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 import { Message } from '../../../../models/message.class';
 import { FormsModule } from '@angular/forms';
 import { ChannelSelectionService } from '../../../services/channel-selection.service';
+import { FileUploadeService } from '../../../services/file-upload.service';
 
 @Component({
   selector: 'app-thread-message-input',
@@ -14,18 +15,73 @@ import { ChannelSelectionService } from '../../../services/channel-selection.ser
 })
 export class ThreadMessageInputComponent implements OnInit {
   @Input() threadId: any;
+
   message = new Message();
+  weekday: any;
+  year: any;
+  month: any;
+  day: any;
+  hour: any;
+  minute: any;
+  user: any;
+
   currentChannel: any;
+
+  selectedFile: File | null = null;
+  FileUrl: any;
 
   constructor(
     private firestore: Firestore,
-    private channelSelectionService: ChannelSelectionService
+    private channelSelectionService: ChannelSelectionService,
+    private fileUploadeService: FileUploadeService
   ) {}
 
-  async saveMessage() {
-    this.updateDateTime();
-    setTimeout(() => {}, 100);
+  //speichert wercher channel gerade ausgewählt ist
+  ngOnInit(): void {
+    this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
+      this.currentChannel = channel;
+    });
+  }
 
+  //speichert die bilder in den cache
+  async saveFileToCache() {
+    if (this.selectedFile) {
+      const imageUrl = await this.fileUploadeService.uploadFile(
+        this.selectedFile,
+        'messangeCache'
+      );
+      this.FileUrl = imageUrl;
+    } else {
+      console.error('No file selected');
+    }
+  }
+
+  //löscht die bilder aus den cache
+  deleteFile() {
+    let name = this.selectedFile!.name;
+    this.fileUploadeService.deleteFile(name!, 'messangeCache');
+    this.FileUrl = null;
+    this.selectedFile = null;
+  }
+
+  //läd die bilder in den cache wenn sie ausgewählt wurde
+  onFileSelected(event: any) {
+    if (this.FileUrl) {
+      this.deleteFile();
+    }
+    this.selectedFile = event.target.files[0];
+    this.saveFileToCache();
+  }
+
+  //erstannt eine nachricht
+  async saveMessage() {
+    if (this.selectedFile) {
+      await this.saveFile();
+      this.addIMG();
+      this.deleteFile();
+    }
+
+    this.updateDateTime();
     await addDoc(
       collection(
         this.firestore,
@@ -36,16 +92,25 @@ export class ThreadMessageInputComponent implements OnInit {
         'thread'
       ),
       this.toJSON()
-    )
-      .catch((err) => {
-        console.error(err);
-      })
-      .then((docRef) => {
-      });
-
+    ).catch((err) => {
+      console.error(err);
+    });
     this.message.message = '';
   }
 
+  //wenn ein bild ausgewählt ist wird diese ins storage hochgeladen und dessen url in der variable FileUrl gespeichert
+  async saveFile() {
+    if (this.selectedFile) {
+      const imageUrl = await this.fileUploadeService.uploadFile(
+        this.selectedFile,
+        'messangeImages'
+      );
+      console.log(imageUrl);
+      this.FileUrl = imageUrl;
+    }
+  }
+
+  //gibt die vorhandenen informationen als JSON zurück
   toJSON() {
     return {
       id: this.message.id,
@@ -59,9 +124,12 @@ export class ThreadMessageInputComponent implements OnInit {
       seconds: this.message.seconds,
       milliseconds: this.message.milliseconds,
       user: this.message.user,
+      fileUrl: this.message.fileUrl,
+      fileName: this.message.fileName,
     };
   }
 
+  //erstellt die daten der aktuellen zeit
   updateDateTime(): void {
     const now = new Date();
     this.message.weekday = now.toLocaleDateString('de-DE', { weekday: 'long' });
@@ -72,17 +140,12 @@ export class ThreadMessageInputComponent implements OnInit {
     this.message.minute = now.getMinutes();
     this.message.seconds = now.getSeconds();
     this.message.milliseconds = now.getMilliseconds(); // Millisekunden hinzufügen
+  }
+
+  //fügt die restlichen variablen ins model
+  addIMG() {
+    this.message.fileUrl = this.FileUrl;
+    this.message.fileName = this.selectedFile?.name;
     this.message.user = 'send';
-  }
-
-  ngOnInit(): void {
-    this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
-      this.currentChannel = channel;
-      this.onChannelChange(channel);
-    });
-  }
-
-  onChannelChange(channel: string): void {
-    // Deine Logik hier
   }
 }
