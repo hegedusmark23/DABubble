c6823c5b02a68be61d34a3f3cde7ef96323d3072
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-08-04 09:38:49 +0200

    added searchField service and add user to channel component

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -7,12 +7,13 @@ import { OpenSidebarComponent } from "../open-sidebar/open-sidebar.component";
 import { EditProfilComponent } from '../edit-profil/edit-profil.component';
 import { EditProfilContactformComponent } from '../edit-profil-contactform/edit-profil-contactform.component';
 import { UserProfilComponent } from '../user-profil/user-profil.component';
+import { AddUserToChannelComponent } from '../add-user-to-channel/add-user-to-channel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent, SidebarComponent, ChannelComponent, CreateChannelComponent, OpenSidebarComponent, 
-    EditProfilComponent, EditProfilContactformComponent, UserProfilComponent],
+    EditProfilComponent, EditProfilContactformComponent, UserProfilComponent, AddUserToChannelComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
