17af3f136be4d51baf878b61e9666c3cb2120fe6
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-08-13 16:47:33 +0200

    added channel title

--- src/app/main/channel/channel-header/channel-header.component.ts
+++ src/app/main/channel/channel-header/channel-header.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EditChannelComponent } from '../../edit-channel/edit-channel.component';
 import { EditChannelService } from '../../../services/edit-channel.service';
 import { ChannelSelectionService } from '../../../services/channel-selection.service';
+import { SidebarService } from '../../../services/sidebar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-header',
   standalone: true,
-  imports: [EditChannelComponent],
+  imports: [EditChannelComponent ,CommonModule],
   templateUrl: './channel-header.component.html',
   styleUrl: './channel-header.component.scss',
 })
 export class ChannelHeaderComponent {
   currentChannel: any;
+  channelInfo = inject(SidebarService);
 
   constructor(
     public editChannelService: EditChannelService,
