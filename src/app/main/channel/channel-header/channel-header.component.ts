49300e45d2ec7a1aacf8ffe338cb442a5e270af5
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-10-28 16:04:22 +0100

    added documentation

--- src/app/main/channel/channel-header/channel-header.component.ts
+++ src/app/main/channel/channel-header/channel-header.component.ts
@@ -43,8 +43,18 @@ export class ChannelHeaderComponent {
     public editChannelService: EditChannelService,
     private channelSelectionService: ChannelSelectionService,
     public chatAreaService: ChatAreaService
-  ) {}
+  ) { }
 
+  /**
+ * Initializes the component by subscribing to the selected channel changes.
+ *
+ * @void
+ *
+ * @description
+ * - Retrieves the currently selected channel using `channelSelectionService`.
+ * - Updates `currentChannelId` with the selected channel.
+ * - Calls `subChannels` to perform additional setup or subscriptions related to the channel.
+ */
   ngOnInit(): void {
     this.channelSelectionService.getSelectedChannel().subscribe((channel) => {
       this.currentChannelId = channel;
@@ -52,14 +62,40 @@ export class ChannelHeaderComponent {
     });
   }
 
+  /**
+ * Opens the dialog to add a user to the current channel.
+ *
+ * @void
+ *
+ * @description
+ * - Sets `addUserFromHeaderToChannelOpen` to true to display the user addition dialog.
+ */
   openAddUserToChannel() {
     this.channelInfo.addUserFromHeaderToChannelOpen = true;
   }
 
+  /**
+ * Opens the user list in the current channel.
+ *
+ * @description
+ *  Sets `openUserList` to true to display the user list interface.
+ */
   openUserList() {
     this.channelInfo.openUserList = true;
   }
 
+  /**
+ * Subscribes to the channels collection in Firestore and updates the current channel.
+ *
+ * @void
+ *
+ * @description
+ * - Queries the `Channels` collection with a limit of 1000 documents.
+ * - Listens for changes in the channels collection using `onSnapshot`.
+ * - Iterates through the channel documents, setting the channel data with `setNoteChannel`.
+ * - Updates `currentChannel` if the channel ID matches `currentChannelId`.
+ * - Calls `setUserNumberBasedOnImages` to update the user number based on images.
+ */
   subChannels() {
     const q = query(collection(this.firestore, 'Channels'), limit(1000));
     onSnapshot(q, (list) => {
@@ -77,6 +113,17 @@ export class ChannelHeaderComponent {
     });
   }
 
+  /**
+ * Updates the user number based on the images in the current channel.
+ *
+ * @void
+ *
+ * @description
+ * - Checks if `AllChannelsImages` exists and if `currentChannelNumber` is defined.
+ * - Retrieves the images for the current channel based on the channel number.
+ * - Sets `userNumber` to the length of `currentChannel.images` if images exist; otherwise, sets it to 0.
+ * - If the conditions are not met, `userNumber` is also set to 0.
+ */
   setUserNumberBasedOnImages() {
     if (
       this.channelInfo.AllChannelsImages &&
@@ -84,7 +131,7 @@ export class ChannelHeaderComponent {
     ) {
       const images =
         this.channelInfo.AllChannelsImages[
-          this.channelInfo.currentChannelNumber
+        this.channelInfo.currentChannelNumber
         ];
       if (images) {
         this.userNumber = this.currentChannel.images.length;
@@ -96,10 +143,26 @@ export class ChannelHeaderComponent {
     }
   }
 
+  /**
+ * Sets the hover state to true.
+ *
+ * @void
+ *
+ * @description
+ * - Updates `divHover` to true to indicate that the element is being hovered over.
+ */
   hover() {
     this.divHover = true;
   }
 
+  /**
+ * Resets the hover state to false.
+ *
+ * @void
+ *
+ * @description
+ * - Updates `divHover` to false to indicate that the element is no longer being hovered over.
+ */
   hoverOff() {
     this.divHover = false;
   }
