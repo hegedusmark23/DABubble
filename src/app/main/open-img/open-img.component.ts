db8dd60f33e39a02b783273f80b0f73f29e880d5
joostheidrich <joost.heidrich@web.de>
2024-11-05 13:18:59 +0100

    img download

--- src/app/main/open-img/open-img.component.ts
+++ src/app/main/open-img/open-img.component.ts
@@ -13,9 +13,15 @@ export class OpenImgComponent implements OnInit {
   src = '';
   constructor(public channelSelectionService: ChannelSelectionService) {}
 
+  imgWidth: any;
+  imgHeight: any;
+
   ngOnInit(): void {
     this.channelSelectionService.getselectedIMG().subscribe((src) => {
       this.src = src;
+      if (src) {
+        this.getImageDimensionBasedOnAspect(src);
+      }
     });
   }
 
@@ -33,16 +39,39 @@ export class OpenImgComponent implements OnInit {
    * Determines whether the provided image source has a greater width or height based on its aspect ratio.
    *
    * @param {string} src - The source URL of the image.
-   * @returns {'width' | 'height'} - Returns 'width' if the image is wider than it is tall, otherwise returns 'height'.
    */
-  getImageDimensionBasedOnAspect(src: string): 'width' | 'height' {
+  getImageDimensionBasedOnAspect(src: string) {
     const img = new Image();
     img.src = src;
+    const width = img.width;
+    const height = img.height;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-    if (img.width > img.height) {
-      return 'width';
-    } else {
-      return 'height';
-    }
+    const maxWidth = screenWidth * 0.8;
+    const maxHeight = screenHeight * 0.8;
+
+    let newWidth = width;
+    let newHeight = height;
+
+    const widthRatio = maxWidth / width;
+    const heightRatio = maxHeight / height;
+    const scalingFactor = Math.min(widthRatio, heightRatio);
+
+    newWidth = width * scalingFactor;
+    newHeight = height * scalingFactor;
+
+    this.imgHeight = newHeight;
+    this.imgWidth = newWidth;
+  }
+
+  downloadImg(src: string) {
+    const link = document.createElement('a');
+    link.href = src;
+    link.target = '_blank'; // Öffnet in einem neuen Tab
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
